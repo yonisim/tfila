11f6cae593c651f2613087314b58e14ad17f8fab
Yonatans <yonatan@pcysys.com>
2022-08-24 18:56:40 +0300

    added auto update script

---
+++ py-scripts/refresh.cmd
@@ -0,0 +1,29 @@
+@ECHO OFF
+:while
+    ping -n 2 -w 700 8.8.8.8 | find "TTL="
+    echo %ERRORLEVEL%
+    IF %ERRORLEVEL% EQU 0 (
+        echo Connected to the internet.
+        goto :pull
+        :continue_update
+        echo updating
+        taskkill /F /im electron.exe
+        npm start
+        goto :continue_loop
+    ) ELSE (
+        echo Not connected to the internet.
+    )
+    :continue_loop
+    timeout 5
+    goto :while
+
+:pull
+git reset --hard
+git pull https://github.com/yonisim/tfila.git | find "bla bla"
+echo %ERRORLEVEL%
+IF %ERRORLEVEL% EQU 1 (
+    goto :continue_update
+) ELSE (
+    echo up to date
+    goto :continue_loop
+)
