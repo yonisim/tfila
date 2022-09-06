9835a9ad5132a7fea495b80c6f2a8f665da62de2
Yonatans <yonatan@pcysys.com>
2022-09-06 22:24:04 +0300

    finally section

--- py-scripts/refresh.cmd
+++ py-scripts/refresh.cmd
@@ -2,13 +2,15 @@
 :while
     ping -n 2 -w 700 8.8.8.8 | find "TTL="
     echo %ERRORLEVEL%
+    echo before main if
     IF %ERRORLEVEL% EQU 0 (
         echo Connected to the internet.
         goto :pull
-        :continue_update
     ) ELSE (
         echo Not connected to the internet.
     )
+    :finally
+    echo before start the process
     goto :start_the_process
     :continue_loop
     timeout 5
@@ -20,6 +22,8 @@ git pull https://github.com/yonisim/tfila.git | find "up to date"
 echo %ERRORLEVEL%
 IF %ERRORLEVEL% EQU 1 (
     goto :kill_the_process
+    :continue_update
+    goto :finally
 ) ELSE (
     echo up to date
     goto :continue_loop
