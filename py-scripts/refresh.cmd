e9c6a21bc9c8dd3a1632d8ec15ae9b1863e9051e
Yonatans <yonatan@pcysys.com>
2022-09-06 22:06:02 +0300

    kill the process if update is made

--- py-scripts/refresh.cmd
+++ py-scripts/refresh.cmd
@@ -6,13 +6,10 @@
         echo Connected to the internet.
         goto :pull
         :continue_update
-        echo updating
-        taskkill /F /im electron.exe
-        START npm start
-        goto :continue_loop
     ) ELSE (
         echo Not connected to the internet.
     )
+    goto :start_the_process
     :continue_loop
     timeout 5
     goto :while
@@ -22,8 +19,21 @@ git reset --hard
 git pull https://github.com/yonisim/tfila.git | find "up to date"
 echo %ERRORLEVEL%
 IF %ERRORLEVEL% EQU 1 (
-    goto :continue_update
+    goto :kill_the_process
 ) ELSE (
     echo up to date
     goto :continue_loop
 )
+
+:start_the_process
+tasklist  /FI "IMAGENAME eq electron.exe" | find "No tasks are running"
+IF %ERRORLEVEL% EQU 0 (
+    echo starting the process
+    START npm start
+}
+goto :continue_loop
+
+:kill_the_process
+echo killing the process
+taskkill /F /im electron.exe
+goto :continue_update
