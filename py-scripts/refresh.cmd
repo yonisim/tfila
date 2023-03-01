810107dff153cc105efd4b2e407e1ac2a4032f1c
Yonatans <yonatan@pcysys.com>
2023-03-01 23:14:24 +0200

    changed bat to cmd

--- py-scripts/refresh.cmd
+++ py-scripts/refresh.cmd
@@ -6,7 +6,7 @@ CALL :NORMALIZEPATH "..\tfila-data"
 SET DATA_ABSOLUTE_PATH=%RETVAL:\=/%
 SET TFILA_REPO=https://github.com/yonisim/tfila.git
 
-CALL check_for_updates.bat
+CALL check_for_updates.cmd
 
 :NORMALIZEPATH
   SET RETVAL=%~f1
