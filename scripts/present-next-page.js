717784de5aa57fd061f7cf4bed44e5784c142f70
Yonatans <yonatan@pcysys.com>
2023-04-04 00:33:01 +0300

    return advertisements

--- scripts/present-next-page.js
+++ scripts/present-next-page.js
@@ -188,7 +188,7 @@ function get_slide_show_items_ids(){
     //slide_show_items.push('day_times');
     //slide_show_items.push('messages');
     //slide_show_items.push('tormim');
-    //slide_show_items.push('advertisement');
+    slide_show_items.push('advertisement');
     return slide_show_items;
 }
 
