4f22ce84d07072b1146d3d9a1b8d8fb687c8b3ce
Yonatans <yonatan@pcysys.com>
2024-08-11 11:25:24 +0300

    mincha shabat 6

--- scripts/present-next-page.js
+++ scripts/present-next-page.js
@@ -972,7 +972,7 @@ async function present_shabat_prayer_times(current_date){
     document.getElementById("prayer-times-title-parasha").innerText = this_shabat_times['parasha'];
     var shabat_in = this_shabat_times["in"];
     var arvit_shabat = this_shabat_times["out"];
-    var mincha_ktana = '18:30';
+    var mincha_ktana = '18:00';
 
     await show_shabat_eve_times(current_date, shabat_in, 'first_column');
     
