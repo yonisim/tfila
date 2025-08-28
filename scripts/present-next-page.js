a7b909b1132d5818f547521f60d7d43db74e6a76
Yonatans <yonatan@pcysys.com>
2025-08-28 15:19:36 +0300

    siftei renanot

--- scripts/present-next-page.js
+++ scripts/present-next-page.js
@@ -275,7 +275,7 @@ function is_elul(date){
 }
 
 function is_slihot_days(date){
-    return is_between_dates(date, "2024-09-01T00:00", "2024-09-30T23:00");
+    return is_between_dates(date, "2025-08-26T00:00", "2025-09-30T23:00");
 }
 
 function is_minyan_plag_active(date){
