003a2f7054ea5c23e7c8cc9bbb3908438cbf212c
yonisim <yonisim@users.noreply.github.com>
2026-02-13 09:17:00 +0200

    mincha shabat

--- scripts/present-next-page.js
+++ scripts/present-next-page.js
@@ -1385,7 +1385,7 @@ async function present_shabat_prayer_times(current_date){
     var shabat_in = this_shabat_times["in"];
     var arvit_shabat = this_shabat_times["out"];
     var shacharit_main = '08:15';
-    var mincha_ktana = '16:00';
+    var mincha_ktana = '16:30';
 
     await show_shabat_eve_times(current_date, shabat_in, 'first_column');
 
