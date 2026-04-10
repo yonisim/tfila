7b93d872d5baaa65376fc4b46da98fa568d0c7ab
yonisim <yonisim@users.noreply.github.com>
2026-04-10 09:06:24 +0300

    Pirkey avot

--- scripts/present-next-page.js
+++ scripts/present-next-page.js
@@ -1444,7 +1444,7 @@ async function present_shabat_prayer_times(current_date){
         set_element_html('mincha-ktana', mincha_ktana);
         set_element_html('tehilim', add_minutes_to_time(mincha_ktana, 20));
         set_element_html('shiur-pirkei-avot', add_minutes_to_time(mincha_ktana, 20));
-        hide_element('shiur-pirkei-avot')
+        //hide_element('shiur-pirkei-avot')
 
         set_element_html('arvit-shabat', arvit_shabat);
         set_element_html('arvit-shabat-2', add_minutes_to_time(arvit_shabat, 15));
